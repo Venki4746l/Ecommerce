33fc5409f279918dbd466295ca521786ab3e2b48
Dhanaraju <naramsettydhanaraj@gmail.com>
2022-11-28 11:32:47 +0530

    test

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,11 +2,21 @@ import React, { useState } from "react";
 import Login from "../pages/Login/Login";
 import Vendorpage from "./vendorSection/Vendorpage";
 
+import ArrivedSection from './ArrivedSection/ArrivedSection'
+
+import StayUpdated from "./StayUpdated/StayUpdated";
+
+import TrendySection from './TrendySection/TrendySection'
+
+
 const Home = () => {
   const [loginmodal, setLoginModal] = useState(false);
   return (
     <div>
       <button onClick={()=>setLoginModal(!loginmodal)}>Login</button>
+      <TrendySection />
+      <StayUpdated />
+      <ArrivedSection />
       <Vendorpage />
       <div>{loginmodal && <Login />}</div>
       

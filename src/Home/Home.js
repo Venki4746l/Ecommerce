cea0d7d326ff19628bae79177fcae7743cf1b243
Shiva <dandusaisiva1998@gmail.com>
2022-11-28 12:41:19 +0530

    Shiva

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import Login from "../pages/Login/Login";
 import Vendorpage from "./vendorSection/Vendorpage";
+import Navbar from "./NavbarSection/Navbar";
+import Dropdowns from "./DropDown/Dropdowns";
+import TopSection from "./TopSection/TopSection";
 
 const Home = () => {
   const [loginmodal, setLoginModal] = useState(false);
   return (
     <div>
       <button onClick={()=>setLoginModal(!loginmodal)}>Login</button>
+      <TopSection/>
+      <Navbar/>
+      <Dropdowns/>
       <Vendorpage />
       <div>{loginmodal && <Login />}</div>
       

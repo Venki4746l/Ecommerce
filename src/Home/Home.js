b317f51ea8b35dfe7df5665605cf9ae42d651bd2
venkateswara rao <venkigopisamineni123@gmail.com>
2022-11-29 08:42:44 +0530

    shivaContent

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,44 +1,45 @@
-import React, { useState } from "react";
-import Login from "../pages/Login/Login";
-import Vendorpage from "./vendorSection/Vendorpage";
+import React from "react";
+//import Login from "../pages/Login/Login";
+
 import Navbar from "./NavbarSection/Navbar";
 import Dropdowns from "./DropDown/Dropdowns";
 import TopSection from "./TopSection/TopSection";
 
-import ArrivedSection from './ArrivedSection/ArrivedSection'
+// import Vendorpage from "./vendorSection/Vendorpage";
 
-import StayUpdated from "./StayUpdated/StayUpdated";
+// import ArrivedSection from './ArrivedSection/ArrivedSection'
 
-import TrendySection from './TrendySection/TrendySection'
-import Feature from "./Features/Features"; 
-import Product from './Products/Product';
-import Footer from "../Footer/Footer"
-import Cards from "./CardOffer/Cards"
+// import StayUpdated from "./StayUpdated/StayUpdated";
+
+// import TrendySection from './TrendySection/TrendySection'
+// import Feature from "./Features/Features";
+// import Product from './Products/Product';
+// import Footer from "../Footer/Footer"
+// import Cards from "./CardOffer/Cards"
 
 const Home = () => {
-  const [loginmodal, setLoginModal] = useState(false);
-  const modalClose =(info)=>{
-    setLoginModal(info)
-  }
+  // const [loginmodal, setLoginModal] = useState(false);
+  // const modalClose =(info)=>{
+  //   setLoginModal(info)
+  // }
   return (
     <div>
-      <button onClick={()=>setLoginModal(!loginmodal)}>Login</button>
-      <TopSection/>
-      <Navbar/>
-      <Dropdowns/>
-     
-      <div>{loginmodal && <Login modalClose={modalClose} />}</div>
-      <Feature />
+      {/* <button onClick={()=>setLoginModal(!loginmodal)}>Login</button> */}
+      <TopSection />
+      <div className="d-flex">
+        <Dropdowns />
+        <Navbar />
+      </div>
+
+      {/* <div>{loginmodal && <Login modalClose={modalClose} />}</div> */}
+      {/* <Feature />
       <Product />
       <Cards/>
       <TrendySection />
       <StayUpdated />
       <ArrivedSection />
       <Vendorpage />
-      <Footer/>
-     
-      
-      
+      <Footer/> */}
     </div>
   );
 };

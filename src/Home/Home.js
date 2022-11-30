54d8b310888a2acba8c7372d86e21c1cde1ed002
venkateswara rao <venkigopisamineni123@gmail.com>
2022-11-30 12:22:26 +0530

    login modal

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,5 @@
-import React from "react";
-//import Login from "../pages/Login/Login";
+import React,{useState} from "react";
+import Login from "../pages/Login/Login";
 
 import Navbar from "./NavbarSection/Navbar";
 import Dropdowns from "./DropDown/Dropdowns";
@@ -15,20 +15,20 @@ import Vendorpage from "./vendorSection/Vendorpage";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
-  // const [loginmodal, setLoginModal] = useState(false);
-  // const modalClose =(info)=>{
-  //   setLoginModal(info)
-  // }
+  const [loginmodal, setLoginModal] = useState(false);
+  const modalClose =(info)=>{
+    setLoginModal(info)
+  }
   return (
     <div>
-      {/* <button onClick={()=>setLoginModal(!loginmodal)}>Login</button> */}
+      <button onClick={()=>setLoginModal(!loginmodal)}>Login</button>
       <TopSection />
       <div className="d-flex">
         <Dropdowns />
         <Navbar />
       </div>
 
-      {/* <div>{loginmodal && <Login modalClose={modalClose} />}</div> */}
+      <div>{loginmodal && <Login modalClose={modalClose} />}</div>
       
       <Cards />
       <TrendySection />

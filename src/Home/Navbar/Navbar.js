60544b54dd57803c61cd7bf104e7f3cbe24a93fa
venkateswara rao <venkigopisamineni123@gmail.com>
2022-12-02 17:14:37 +0530

    Navbar

--- src/Home/Navbar/Navbar.js
+++ src/Home/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Navbar.css";
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Dropdown from 'react-bootstrap/Dropdown';
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Dropdown from "react-bootstrap/Dropdown";
 
 const Navbar = () => {
   return (
@@ -25,8 +25,9 @@ const Navbar = () => {
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
+                viewBox="0 0 320 512"
                 className="m-3"
+                style={{ height: "20px", width: "80px" }}
               >
                 <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
               </svg>
@@ -73,8 +74,31 @@ const Navbar = () => {
                       Baby's Dresses
                     </a>
                   </div>
-<<<<<<< HEAD
-                </div>
+                </div> */}
+                <Dropdown>
+                  <Dropdown.Toggle variant="white" id="dropdown-basic">
+                    Dresses
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 320 512"
+                      className="m-3"
+                      style={{ height: "20px", width: "80px" }}
+                    >
+                      <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                    </svg>
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">
+                      Men's Dresses
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">
+                      Women's Dresses
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                      Baby's Dresses
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
                 <a href="kd" className="nav-item nav-link">
                   Shirts
                 </a>
@@ -102,35 +126,6 @@ const Navbar = () => {
                 <a href="kd" className="nav-item nav-link">
                   Shoes
                 </a>
-=======
-                </div> */}
-    <Dropdown>
-      <Dropdown.Toggle variant="white" id="dropdown-basic">Dresses
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 320 512"
-          className="m-3"
-          style={{height:"20px",width:"80px"}}
-          >
-          <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
-        </svg>
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Men's Dresses</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Women's Dresses</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Baby's Dresses</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-                <a href="kd" className="nav-item nav-link">Shirts</a>
-                <a href="kd" className="nav-item nav-link">Jeans</a>
-                <a href="kd" className="nav-item nav-link">Swimwear</a>
-                <a href="kd" className="nav-item nav-link">Sleepwear</a>
-                <a href="kd" className="nav-item nav-link">Sportswear</a>
-                <a href="kd" className="nav-item nav-link">Jumpsuits</a>
-                <a href="kd" className="nav-item nav-link">Blazers</a>
-                <a href="kd" className="nav-item nav-link">Jackets</a>
-                <a href="kd" className="nav-item nav-link">Shoes</a>
->>>>>>> origin/Shiva
               </div>
             </nav>
           </div>
@@ -156,7 +151,6 @@ const Navbar = () => {
                 className="collapse navbar-collapse justify-content-between d-lg-block"
                 id="navbarCollapse"
               >
-<<<<<<< HEAD
                 <div className="navbar-nav mr-auto py-0">
                   <a href="index.html" className="nav-item nav-link items">
                     Home
@@ -167,40 +161,18 @@ const Navbar = () => {
                   <a href="detail.html" className="nav-item nav-link items">
                     Shop Detail
                   </a>
-                  <div class="nav-item dropdown">
-                    <a
-                      href="#k"
-                      className="nav-link dropdown-toggle items"
-                      data-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      Pages
-                    </a>
-                    <div className="dropdown-menu rounded-0 m-0">
-                      <a href="cart.html" className="dropdown-item items">
-                        Shopping Cart
-                      </a>
-                      <a href="checkout.html" className="dropdown-item items">
-                        Checkout
-                      </a>
-                    </div>
-                  </div>
+                  <NavDropdown title="Pages" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">
+                      Shopping Cart
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Checkout
+                    </NavDropdown.Item>
+                  </NavDropdown>
                   <a href="contact.html" className="nav-item nav-link items">
                     Contact
                   </a>
                 </div>
-=======
-          <div className="navbar-nav mr-auto py-0">
-            <a href="index.html" className="nav-item nav-link items">Home</a>
-            <a href="shop.html" className="nav-item nav-link items">Shop</a>
-            <a href="detail.html" className="nav-item nav-link items">Shop Detail</a>
-              <NavDropdown title="Pages" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Shopping Cart</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Checkout</NavDropdown.Item>
-              </NavDropdown>
-            <a href="contact.html" className="nav-item nav-link items">Contact</a>
-          </div>
->>>>>>> origin/Shiva
                 <div className="navbar-nav ml-auto py-0">
                   <a href="kd" className="nav-item nav-link items">
                     Login
@@ -291,4 +263,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-

e4ba1b788181eec642e3be8a03cefacd9d6b8d26
venkateswara rao <venkigopisamineni123@gmail.com>
2022-12-02 10:38:50 +0530

    changed

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,26 +4,15 @@ import React from "react";
 import Topbar from "./TopBar/Topbar";
 import Navbar from "./Navbar/Navbar";
 
-import Cards from "./CardOffer/Cards";
-import TrendySection from "./TrendySection/TrendySection";
-import StayUpdated from "./StayUpdated/StayUpdated";
-
-import ArrivedSection from "./ArrivedSection/ArrivedSection";
 
 import Vendorpage from "./vendorSection/Vendorpage";
-import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Topbar />
       <Navbar /> 
-      <Cards />
-      <TrendySection />
-      <StayUpdated />
-      <ArrivedSection />
       <Vendorpage />
-      <Footer />
     </div>
   );
 };

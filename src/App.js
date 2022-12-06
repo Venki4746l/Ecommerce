8d437fd2f48ea9973ddc8440232e6c16d16c29b4
Sivapy9 <dandusaisiva1998@gmail.com>
2022-12-06 11:25:55 +0530

    Newtopbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-//import { Navbar } from 'react-bootstrap'
-//import Login from './pages/Login/Login'
 import Navbar from './Home/Navbar/Navbar';
 import TopNavbar from './Home/TopBar/Topbar';
 const App = () => {

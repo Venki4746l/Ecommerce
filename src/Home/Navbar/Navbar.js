edb8089e39a167427a528c60d5b284227cbf38ae
venkateswara rao <venkigopisamineni123@gmail.com>
2022-12-13 15:13:16 +0530

    navbar

--- src/Home/Navbar/Navbar.js
+++ src/Home/Navbar/Navbar.js
@@ -18,11 +18,7 @@ const Navbar = () => {
             <a
               className="btn shadow-none d-flex align-items-center justify-content-between text-white w-100"
               data-toggle="collapse"
-<<<<<<< HEAD
               href="..."
-              style={{height:"65px",backgroundColor:"#D19C97",marginTop:"-1px",padding:"0 30px",ariaExpanded:"true",}}
-=======
-              href="#navbar-vertical"
               style={{
                 height: "65px",
                 backgroundColor: "#D19C97",
@@ -30,7 +26,6 @@ const Navbar = () => {
                 padding: "0 30px",
                 ariaExpanded: "true",
               }}
->>>>>>> origin/Venkateswara_rao
             >
               <h6 className="m-4 text-dark">Categories</h6>
 
@@ -66,61 +61,38 @@ const Navbar = () => {
                     </svg> */}
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
-<<<<<<< HEAD
                     <Dropdown.Item href="...">Men's Dresses</Dropdown.Item>
                     <Dropdown.Item href="...">Women's Dresses</Dropdown.Item>
                     <Dropdown.Item href="...">Baby's Dresses</Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
-                  <a href="..." className="nav-item nav-link">Shirts</a>
-                  <a href="..." className="nav-item nav-link">Jeans</a>
-                  <a href="..." className="nav-item nav-link">Swimwear</a>
-                  <a href="..." className="nav-item nav-link">Sleepwear</a>
-                  <a href="..." className="nav-item nav-link">Sportswear</a>
-                  <a href="..." className="nav-item nav-link">Jumpsuits</a>
-                  <a href="..." className="nav-item nav-link">Blazers</a>
-                  <a href="..." className="nav-item nav-link">Jackets</a>
-                  <a href="..." className="nav-item nav-link">Shoes</a>
-=======
-                    <Dropdown.Item href="#/action-1">
-                      Men's Dresses
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">
-                      Women's Dresses
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">
-                      Baby's Dresses
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-                <a href="kd" className="nav-item nav-link">
+                <a href="..." className="nav-item nav-link">
                   Shirts
                 </a>
-                <a href="kd" className="nav-item nav-link">
+                <a href="..." className="nav-item nav-link">
                   Jeans
                 </a>
-                <a href="kd" className="nav-item nav-link">
+                <a href="..." className="nav-item nav-link">
                   Swimwear
                 </a>
-                <a href="kd" className="nav-item nav-link">
+                <a href="..." className="nav-item nav-link">
                   Sleepwear
                 </a>
-                <a href="kd" className="nav-item nav-link">
+                <a href="..." className="nav-item nav-link">
                   Sportswear
                 </a>
-                <a href="kd" className="nav-item nav-link">
+                <a href="..." className="nav-item nav-link">
                   Jumpsuits
                 </a>
-                <a href="kd" className="nav-item nav-link">
+                <a href="..." className="nav-item nav-link">
                   Blazers
                 </a>
-                <a href="kd" className="nav-item nav-link">
+                <a href="..." className="nav-item nav-link">
                   Jackets
                 </a>
-                <a href="kd" className="nav-item nav-link">
+                <a href="..." className="nav-item nav-link">
                   Shoes
                 </a>
->>>>>>> origin/Venkateswara_rao
               </div>
             </nav>
           </div>
@@ -149,54 +121,33 @@ const Navbar = () => {
                 id="navbarCollapse"
               >
                 <div className="navbar-nav mr-auto py-0">
-<<<<<<< HEAD
-                  <a href="..." className="nav-item nav-link items">Home</a>
-                  <a href="..." className="nav-item nav-link items">Shop</a>
-                  <a href="..." className="nav-item nav-link items">Shop Detail</a>
-                  <NavDropdown title="Pages" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="...">Shopping Cart</NavDropdown.Item>
-                    <NavDropdown.Item href="...">Checkout</NavDropdown.Item>
-                  </NavDropdown>
-                  <a href="..." className="nav-item nav-link items">Contact</a>
-=======
-                  <a href="index.html" className="nav-item nav-link items">
+                  <a href="..." className="nav-item nav-link items">
                     Home
                   </a>
-                  <a href="shop.html" className="nav-item nav-link items">
+                  <a href="..." className="nav-item nav-link items">
                     Shop
                   </a>
-                  <a href="detail.html" className="nav-item nav-link items">
+                  <a href="..." className="nav-item nav-link items">
                     Shop Detail
                   </a>
                   <NavDropdown title="Pages" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">
+                    <NavDropdown.Item href="...">
                       Shopping Cart
                     </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                      Checkout
-                    </NavDropdown.Item>
+                    <NavDropdown.Item href="...">Checkout</NavDropdown.Item>
                   </NavDropdown>
-                  <a href="contact.html" className="nav-item nav-link items">
+                  <a href="..." className="nav-item nav-link items">
                     Contact
                   </a>
->>>>>>> origin/Venkateswara_rao
                 </div>
-                
+
                 <div className="navbar-nav ml-auto py-0">
-<<<<<<< HEAD
-                  <a href="..." className="nav-item nav-link items">Login</a>
-                  <a href="..." className="nav-item nav-link items">Register</a>
-=======
-                  <button
-                    onClick={() => setLogin(true)}
-                    className="border-0 nav-item nav-link items"
-                  >
+                  <a href="..." className="nav-item nav-link items">
                     Login
-                  </button>
-                  <a href=".." className="nav-item nav-link items">
+                  </a>
+                  <a href="..." className="nav-item nav-link items">
                     Register
                   </a>
->>>>>>> origin/Venkateswara_rao
                 </div>
                 {isLogin && <Login modalClose={modalClosehandler} />}
               </div>
@@ -209,15 +160,6 @@ const Navbar = () => {
             >
               <div className="carousel-inner">
                 <div className="carousel-item active">
-<<<<<<< HEAD
-                  <img src="https://technext.github.io/eshopper/img/carousel-1.jpg" className="d-block w-100" alt="Images"/>
-                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                      <div className="p-3" style={{ maxWidth: "700px" }}>
-                        <h4 className="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
-                          <h3 className="display-4 text-white font-weight-bold mb-4">Reasonable Price</h3>
-                            <a href="..." className="btn btn-light py-2 px-3">Shop Now</a>
-                      </div>
-=======
                   <img
                     src="https://technext.github.io/eshopper/img/carousel-1.jpg"
                     className="d-block w-100"
@@ -231,71 +173,60 @@ const Navbar = () => {
                       <h3 className="display-4 text-white font-weight-bold mb-4">
                         Reasonable Price
                       </h3>
-                      <a href="kd" className="btn btn-light py-2 px-3">
+                      <a href="..." className="btn btn-light py-2 px-3">
                         Shop Now
                       </a>
->>>>>>> origin/Venkateswara_rao
-                    </div>
-                  </div>
-                </div>
-                <div className="carousel-item">
-<<<<<<< HEAD
-                  <img src="https://technext.github.io/eshopper/img/carousel-2.jpg" className="d-block w-100" alt="Images"/>
-                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                      <div className="p-3" style={{ maxWidth: "700px" }}>
-                        <h4 className="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
-                          <h3 className="display-4 text-white font-weight-bold mb-4">Fashionable Dress</h3>
-                            <a href="..." className="btn btn-light py-2 px-3">Shop Now</a>
-                      </div>
-=======
-                  <img
-                    src="https://technext.github.io/eshopper/img/carousel-2.jpg"
-                    className="d-block w-100"
-                    alt="Images"
-                  />
-                  <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                    <div className="p-3" style={{ maxWidth: "700px" }}>
-                      <h4 className="text-light text-uppercase font-weight-medium mb-3">
-                        10% Off Your First Order
-                      </h4>
-                      <h3 className="display-4 text-white font-weight-bold mb-4">
-                        Fashionable Dress
-                      </h3>
-                      <a href="kd" className="btn btn-light py-2 px-3">
-                        Shop Now
-                      </a>
->>>>>>> origin/Venkateswara_rao
                     </div>
                   </div>
                 </div>
               </div>
-
-              <a
-                className="carousel-control-prev"
-                href="#carouselExampleControls"
-                role="button"
-                data-slide="prev"
-              >
-                <span
-                  className="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="sr-only">Previous</span>
-              </a>
-              <a
-                className="carousel-control-next"
-                href="#carouselExampleControls"
-                role="button"
-                data-slide="next"
-              >
-                <span
-                  className="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="sr-only">Next</span>
-              </a>
+              <div className="carousel-item">
+                <img
+                  src="https://technext.github.io/eshopper/img/carousel-2.jpg"
+                  className="d-block w-100"
+                  alt="Images"
+                />
+                <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                  <div className="p-3" style={{ maxWidth: "700px" }}>
+                    <h4 className="text-light text-uppercase font-weight-medium mb-3">
+                      10% Off Your First Order
+                    </h4>
+                    <h3 className="display-4 text-white font-weight-bold mb-4">
+                      Fashionable Dress
+                    </h3>
+                    <a href="..." className="btn btn-light py-2 px-3">
+                      Shop Now
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Next</span>
+          </a>
         </div>
       </div>
     </div>

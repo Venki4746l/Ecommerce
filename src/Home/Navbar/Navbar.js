32487b462e5cf7c4968776088b19029a75f0aa5b
venkateswara rao <venkigopisamineni123@gmail.com>
2022-12-14 09:20:25 +0530

    navbar

--- src/Home/Navbar/Navbar.js
+++ src/Home/Navbar/Navbar.js
@@ -40,10 +40,10 @@ const Navbar = () => {
             >
               <div className="navbar-nav w-100 overflow-hidden navslide" style={{ height: "410px" }}>
                 <Dropdown>
-                  <Dropdown.Toggle variant="white" id="dropdown-basic">Dresses
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="m-3" style={{height:"20px",width:"80px"}}>
+                  <Dropdown.Toggle variant="white" id="dropdown-basic">Dresses 
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="m-3" style={{height:"20px",width:"80px"}}>
                       <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
-                    </svg>
+                    </svg> */}
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
                     <Dropdown.Item href="#/action-1">Men's Dresses</Dropdown.Item>

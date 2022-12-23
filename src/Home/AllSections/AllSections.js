4340a4a40fb5ad3ab83f29b8af64c5a53b09f9fe
Dhanaraju <naramsettydhanaraj@gmail.com>
2022-12-23 11:46:08 +0530

    text

--- src/Home/AllSections/AllSections.js
+++ src/Home/AllSections/AllSections.js
@@ -1,108 +1,115 @@
 import React from "react";
+
 import "./AllSections.css";
+
+
+import TrendySection from "../TrendySection/TrendySection";
 import StayUpdated from "../StayUpdated/StayUpdated";
-import ProductCard from "../ProductCard/ProductCard";
 
 const data = [
   {
     id: 1,
     imgSrc: "https://technext.github.io/eshopper/img/product-1.jpg",
-    title: "womens skirts",
-    price: 456,
-    cross_price: 800,
+    title: "Colorful Stylish Shirt",
+    price: 123.0,
+    cross_price: 123,
   },
   {
     id: 2,
     imgSrc: "https://technext.github.io/eshopper/img/product-2.jpg",
-    title: "kids Stylish Shirt",
-    price: 999,
-    cross_price: 1200,
+    title: "Colorful Stylish Shirt",
+    price: 123,
+    cross_price: 123,
   },
   {
     id: 3,
     imgSrc: "https://technext.github.io/eshopper/img/product-3.jpg",
-    title: "men Stylish Shirt",
-    price: 678,
-    cross_price: 1399,
+    title: "Colorful Stylish Shirt",
+    price: 123,
+    cross_price: 123,
   },
-
   {
     id: 4,
     imgSrc: "https://technext.github.io/eshopper/img/product-4.jpg",
-    title: "Colorful Top",
-    price: 230,
-    cross_price: 568,
+    title: "Colorful Stylish Shirt",
+    price: 123,
+    cross_price: 123,
   },
   {
     id: 5,
     imgSrc: "https://technext.github.io/eshopper/img/product-5.jpg",
     title: "Colorful Stylish Shirt",
     price: 123,
-    cross_price: 444,
+    cross_price: 123,
   },
   {
     id: 6,
     imgSrc: "https://technext.github.io/eshopper/img/product-6.jpg",
-    title: "men court",
-    price: 567,
-    cross_price: 1233,
+    title: "Colorful Stylish Shirt",
+    price: 123,
+    cross_price: 123,
   },
   {
     id: 7,
     imgSrc: "https://technext.github.io/eshopper/img/product-7.jpg",
-    title: "ladies full dress",
+    title: "Colorful Stylish Shirt",
     price: 123,
-    cross_price: 193,
+    cross_price: 123,
   },
   {
     id: 8,
     imgSrc: "https://technext.github.io/eshopper/img/product-8.jpg",
-    title: "Kids wear",
-    price: 673,
-    cross_price: 1110,
+    title: "Colorful Stylish Shirt",
+    price: 123,
+    cross_price: 123,
   },
 ];
 
 const AllSections = () => {
   return (
     <>
-      <div className="heading-section">
-        <h2 className="decorated">
+      <div className="pt-5">
+        
+        <h2 class="decorated">
           <span>Trendy Products</span>
         </h2>
+      
       </div>
-      <div className="d-flex flex-wrap p-sm-1 p-xl-5">
-        {data.map((item) => (
-          <ProductCard key={item.id} productDetails={item} />
-        ))}
-      </div>
-      <StayUpdated />
-      <div className="heading-section">
-        <h2 className="decorated">
-          <span>Just Arrived</span>
-        </h2>
-      </div>
-<<<<<<< HEAD
-      <div className="container-fluid app-container">
+      <div className="container-fluid trendy-shop-container">
         <div className="row">
           {data.map((each) => {
             return (
-              <div className="col-lg-3 col-md-6 col-sm-12">
-                <ArrivedSection key={each.id} shopDetails={each} />
-            </div>
-
-
-
-
-            )
+              <div
+                key={each.id}
+                className="col-lg-4 col-xl-3 col-md-6 col-sm-12"
+              >
+                <TrendySection key={each.id} shopDetails={each} />
+              </div>
+            );
           })}
         </div>
-=======
-      <div className="d-flex flex-wrap p-sm-1 p-lg-5">
-        {data.map((item) => (
-          <ProductCard key={item.id} productDetails={item} />
-        ))}
->>>>>>> Venkateswara_rao
+      </div>
+      <StayUpdated />
+      <div className="pt-5">
+        
+        <h2 class="decorated">
+          <span>Just Arrived</span>
+        </h2>
+      
+      </div>
+      <div className="container-fluid trendy-shop-container">
+        <div className="row">
+          {data.map((each) => {
+            return (
+              <div
+                key={each.id}
+                className="col-lg-4 col-xl-3 col-md-6 col-sm-12"
+              >
+                <TrendySection key={each.id} shopDetails={each} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
--- src/Home/vendorSection/Vendor.js
+++ src/Home/vendorSection/Vendor.js
@@ -15,6 +15,7 @@ const Vendor = () => {
           </div>
         ))}
       </OwlCarousel>
+  
     </div>
   );
 };

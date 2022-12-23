6bafdaa132f4a050d205e1ab6cabc9f16030221f
venkateswara rao <venkigopisamineni123@gmail.com>
2022-12-23 11:11:52 +0530

    updation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
+import Home from './Home/Home'
 //import Checkout from './components/CheckOut/Checkout'
-import Shopingcarddipslay from './components/ShopingCart/Shopingcarddipslay'
+//import Shopingcarddipslay from './components/ShopingCart/Shopingcarddipslay'
 //import AllSections from './Home/AllSections/AllSections'
 //import Login from './pages/Login/Login'
-import Home from './Home/Home'
+//import Home from './Home/Home'
+//import Login from './pages/Login/Login'
 
 
 const App = () => {
   return (
     <>
-    <Home/>
-    <Shopingcarddipslay />
-   
+<Home />   
     </>
   )
 }
--- src/Home/Navbar/Navbar.js
+++ src/Home/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CarsoulImages from "./CarsoulImages";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -6,15 +7,26 @@ const Navbar = () => {
     <div>
       <div className="container-fluid mb-5">
         <div className="row border-top px-xl-5">
-          <div className="col-lg-3 d-none d-lg-block">
+          <div className="col-lg-3 cateegioressection  d-none d-lg-block">
             <a
               className="btn shadow-none d-flex align-items-center justify-content-between text-white w-100"
               data-toggle="collapse"
               href="#navbar-vertical"
-              style={{height: "65px", backgroundColor: "#D19C97", marginTop: "-1px", padding: "0 10px", ariaExpanded: "true",}}
+              style={{
+                height: "65px",
+                backgroundColor: "#D19C97",
+                marginTop: "-1px",
+                padding: "0 10px",
+                ariaExpanded: "true",
+              }}
             >
               <h6 className="font-weight-bold m-4 text-dark">Categories</h6>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="m-3" style={{height:'20px',width:'20px'}}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                className="m-3"
+                style={{ height: "20px", width: "20px" }}
+              >
                 <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
               </svg>
             </a>
@@ -28,9 +40,21 @@ const Navbar = () => {
                 style={{ height: "410px" }}
               >
                 <div className="nav-item dropdown">
-                  <a href="..." className="font-weight-bold nav-link" data-toggle="dropdown" aria-expanded="false" role="button" id="navbarDropdown">
+                  <a
+                    href="..."
+                    className="font-weight-bold nav-link"
+                    data-toggle="dropdown"
+                    aria-expanded="false"
+                    role="button"
+                    id="navbarDropdown"
+                  >
                     Dresses
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="m-3" style={{height:"10px",width:"40px"}}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 320 512"
+                      className="m-3"
+                      style={{ height: "10px", width: "40px" }}
+                    >
                       <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
                     </svg>
                   </a>
@@ -38,29 +62,54 @@ const Navbar = () => {
                     className="dropdown-menu position-absolute bg-light border-0 rounded-0 w-100 m-0"
                     aria-labelledby="navbarDropdown"
                   >
-                    <a href="..." className="dropdown-item">Men's Dresses</a>
-                    <a href="..." className="dropdown-item">Women's Dresses</a>
-                    <a href="..." className="dropdown-item">Baby's Dresses</a>
+                    <a href="..." className="dropdown-item">
+                      Men's Dresses
+                    </a>
+                    <a href="..." className="dropdown-item">
+                      Women's Dresses
+                    </a>
+                    <a href="..." className="dropdown-item">
+                      Baby's Dresses
+                    </a>
                   </div>
                 </div>
-                <a href="..." className="font-weight-bold nav-item nav-link">Shirts</a>
-                <a href="..." className="font-weight-bold nav-item nav-link">Jeans</a>
-                <a href="..." className="font-weight-bold nav-item nav-link">Swimwear</a>
-                <a href="..." className="font-weight-bold nav-item nav-link">Sleepwear</a>
-                <a href="..." className="font-weight-bold nav-item nav-link">Sportswear</a>
-                <a href="..." className="font-weight-bold nav-item nav-link">Jumpsuits</a>
-                <a href="..." className="font-weight-bold nav-item nav-link">Blazers</a>
-                <a href="..." className="font-weight-bold nav-item nav-link">Jackets</a>
-                <a href="..." className="font-weight-bold nav-item nav-link">Shoes</a>
+                <a href="..." className="font-weight-bold nav-item nav-link">
+                  Shirts
+                </a>
+                <a href="..." className="font-weight-bold nav-item nav-link">
+                  Jeans
+                </a>
+                <a href="..." className="font-weight-bold nav-item nav-link">
+                  Swimwear
+                </a>
+                <a href="..." className="font-weight-bold nav-item nav-link">
+                  Sleepwear
+                </a>
+                <a href="..." className="font-weight-bold nav-item nav-link">
+                  Sportswear
+                </a>
+                <a href="..." className="font-weight-bold nav-item nav-link">
+                  Jumpsuits
+                </a>
+                <a href="..." className="font-weight-bold nav-item nav-link">
+                  Blazers
+                </a>
+                <a href="..." className="font-weight-bold nav-item nav-link">
+                  Jackets
+                </a>
+                <a href="..." className="font-weight-bold nav-item nav-link">
+                  Shoes
+                </a>
               </div>
             </nav>
-            
           </div>
           <div className="col-lg-9">
             <nav className="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
               <a href="..." className="text-decoration-none d-block d-lg-none">
                 <h1 className="m-0 display-5 font-weight-semi-bold">
-                  <span className="text-primary font-weight-bold border px-3 mr-1">E</span>
+                  <span className="text-primary font-weight-bold border px-3 mr-1">
+                    E
+                  </span>
                   <span className="heading">Shopper</span>
                 </h1>
               </a>
@@ -75,12 +124,27 @@ const Navbar = () => {
               <div
                 className="collapse navbar-collapse justify-content-between d-lg-block"
                 id="navbarCollapse"
-                style={{height: "65px"}}
+                style={{ height: "65px" }}
               >
                 <div className="navbar-nav mr-auto py-0">
-                  <a href="..." className="font-weight-bold nav-item nav-link items">Home</a>
-                  <a href="..." className="font-weight-bold nav-item nav-link items">Shop</a>
-                  <a href="..." className="font-weight-bold nav-item nav-link items">Shop Detail</a>
+                  <a
+                    href="..."
+                    className="font-weight-bold nav-item nav-link items"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="..."
+                    className="font-weight-bold nav-item nav-link items"
+                  >
+                    Shop
+                  </a>
+                  <a
+                    href="..."
+                    className="font-weight-bold nav-item nav-link items"
+                  >
+                    Shop Detail
+                  </a>
                   <div className="font-weight-bold nav-item dropdown">
                     <a
                       href="..."
@@ -91,53 +155,36 @@ const Navbar = () => {
                       Pages
                     </a>
                     <div className="dropdown-menu rounded-0 m-0">
-                      <a href="..." className="dropdown-item items">Shopping Cart</a>
-                      <a href="..." className="dropdown-item items">Checkout</a>
+                      <a href="..." className="dropdown-item items">
+                        Shopping Cart
+                      </a>
+                      <a href="..." className="dropdown-item items">
+                        Checkout
+                      </a>
                     </div>
                   </div>
-                  <a href="contact.html" className="font-weight-bold nav-item nav-link items">Contact</a>
+                  <a
+                    href="contact.html"
+                    className="font-weight-bold nav-item nav-link items"
+                  >
+                    Contact
+                  </a>
                 </div>
                 <div className="navbar-nav ml-auto py-0">
-                  <a href="..." className="font-weight-bold nav-item nav-link items">Login</a>
-                  <a href="..." className="font-weight-bold nav-item nav-link items">Register</a>
+                  <a href="/Login" className="font-weight-bold border-none nav-item nav-link items">
+                    Login
+                  </a>
+                  <a
+                    href="..."
+                    className="font-weight-bold nav-item nav-link items"
+                  >
+                    Register
+                  </a>
                 </div>
               </div>
             </nav>
 
-            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-      <div className="carousel-inner">
-      <div className="carousel-item active">
-      <img src="https://technext.github.io/eshopper/img/carousel-1.jpg" className="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-        <div class="p-3" style={{ maxWidth: "700px" }}>
-          <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
-            <h3 class="display-4 text-white font-weight-bold mb-4">Reasonable Price</h3>
-              <a href="..." class="btn btn-light py-2 px-3">Shop Now</a>
-        </div>
-      </div>
-      </div>
-      
-      <div className="carousel-item">
-      <img src="https://technext.github.io/eshopper/img/carousel-2.jpg" className="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-        <div class="p-3" style={{ maxWidth: "700px" }}>
-          <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
-            <h3 class="display-4 text-white font-weight-bold mb-4">Fashionable Dress</h3>
-              <a href="..." class="btn btn-light py-2 px-3">Shop Now</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-
+            <CarsoulImages />
           </div>
         </div>
       </div>
